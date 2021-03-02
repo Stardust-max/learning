2e0b0b76023ad4c41ff147e0f0595116c052b2c1
Stardust-max <63161799+Stardust-max@users.noreply.github.com>
2021-03-02 22:56:02 +0800

    Add files via upload

---
+++ PDA/t22.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void){
+    int n;
+    cin >> n;
+    int h = 0;
+    for(int i = 0; i < n; i++){
+    	int p1, p2;
+    	cin >> p1 >> p2;
+    	if(p1 >= 90 && p1 <= 140 && p2 >= 60 && p2 <= 90) h++;
+    	else h = 0;
+	}
+	cout << h << endl;    
+    return 0;
+}
---
+++ PDA/t25.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(void){
+    int M, N, X;
+    cin >> M >> N >> X;
+    int m,n;
+    m = 0;
+	n = N;
+	while(X--){//应该用X--，如果X==0，直接跳出循环
+		//while(m < M){此处的while循环是不必要的，因为每次结束后m均==0，m<M是必然的	
+		m += n;
+		//}
+		//if (m > M) m = M;此处的m=M和后面的for循环都是不必要的，直接考虑进行战斗的结果是战斗力增加M/n即可
+		//for(int i = 1; i <= M/n; i++){
+		if( m>=M ){
+			n += M/n;
+			m = 0;
+		} 		
+		//}
+	}
+	cout << n << endl;
+	return 0;
+}
