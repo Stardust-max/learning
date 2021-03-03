ccca2ba3aa23d6c5cfc69dfaa0179a2826c54522
Stardust-max <769184374@qq.com>
2021-03-03 22:24:45 +0800

    修改了t25的源文件, 提交了t34.cpp

--- PDA/t34.cpp
+++ PDA/t34.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main(){
     int m, n;
     cin >> m >> n;
-    int A[m][n];
+    int A[101][101];
     int i, j;
-    for(i=0; i<m; i++)for(j=0; j<n; j++) cin >> A[j][i];
-    for(i=0; i<m; i++){
-	for(j=0; j<n; j++) cout << A[i][j] << " ";
+    for(i=0; i<m; i++)for(j=0; j<n; j++) cin >> A[i][j];
+    for(i=0; i<n; i++){
+	for(j=0; j<m; j++) cout << A[j][i] << " ";
 	cout << endl;
     }
     return 0; 
