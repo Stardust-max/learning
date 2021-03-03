4cc8f2e25ffc145152b7fa92c098d0a860dcbb29
Stardust-max <769184374@qq.com>
2021-03-03 13:42:43 +0800

    修改了t32的源文件，增加了最大计数变量

--- PDA/t22.cpp
+++ PDA/t22.cpp
@@ -4,13 +4,14 @@ using namespace std;
 int main(void){
     int n;
     cin >> n;
-    int h = 0;
+    int c=0, h = 0;
     for(int i = 0; i < n; i++){
     	int p1, p2;
     	cin >> p1 >> p2;
     	if(p1 >= 90 && p1 <= 140 && p2 >= 60 && p2 <= 90) h++;
     	else h = 0;
-	}
-	cout << h << endl;    
+	if(c<h) c=h;
+        }
+    cout << c << endl;    
     return 0;
 }
