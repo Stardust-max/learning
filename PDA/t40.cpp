3b910f4216fb0e478cba12d379b5147b7269d8bb
Stardust-max <769184374@qq.com>
2021-03-04 17:01:08 +0800

    修改了t40的源文件,赋给了s初值0.

--- PDA/t40.cpp
+++ PDA/t40.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 
 int main(){
-    char s[256];
+    char s[256]={0};//初始化置零一定要记住!这里如果没有={0},结果不可预料
     fgets(s, 256, stdin);
     int n=sizeof(s);
     int num=0;
     for(int i=0; i<n; i++){
         if(s[i]>=48 && s[i]<=57)num++;
+	cout << s[i];
     }
     cout << num;
     return 0;	
