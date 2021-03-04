a2655b631b16e980459d96a4ef8d77b99939fa5f
Stardust-max <769184374@qq.com>
2021-03-04 22:26:20 +0800

    修改了t41的源文件,将输入与字母表进行对应,并储存出现次数.

--- PDA/t41.cpp
+++ PDA/t41.cpp
@@ -1,17 +1,25 @@
+/* 找第一个只出现一次的字符 */
 #include<iostream>
+#include<string.h>
 using namespace std;
 char s[100001]={0};
 
 int main(){
     int i,j;
+    int letters[26]={0};
     fgets(s, 100001, stdin);
-    for(i=0; i<sizeof(s); i++){
-        char temp=s[i];
-	for(j=i+1; j<sizeof(s); j++)
-	    if(s[j] == temp) break;
-	    else if(j==sizeof(s)-1){
-	        cout << temp;
+    for(i=0; i<strlen(s); i++)
+        letters[s[i]-'a']++;
+    for(j=0; j<strlen(s); j++)
+	if(letters[s[j]-'a'] == 1) {
+		cout << s[j];
 		return 0;
-	    }
-    }
+	}
+	    
+    cout << "no";
+    return 0;
 }
+/*=============================================================
+ * 思路:建立一个存储26个字母出现次数的数组, 注意该数组存储字母的顺序是与输入的字符串有关的.
+ * 从数组的头开始遍历,第一个等于1的字符就输出
+ * 关键是想到将输入字符与26个字母匹配*/
