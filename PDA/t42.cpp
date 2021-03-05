50974bfa42e712e0f58c65c8552fc481113d042e
Stardust-max <769184374@qq.com>
2021-03-05 21:48:58 +0800

    添加了t42的源文件.

--- PDA/t42.cpp
+++ PDA/t42.cpp
@@ -8,15 +8,31 @@ int main(){
     cin >> N;
     int i;
     char temp1, temp2, temp3;
-    char out[N][7]={0};
+    const char *out[N];
     for(i=0; i<N; i++){
         cin >> temp1;
-	cout << temp1;
+	//cout << temp1;
 	while(1){
-	    cin >> temp2;
-	    if(temp2!=' ') break;
+	    temp2=getchar();
+	    if(temp2 ==' ') break;
 	}
-	cout << temp2;
+	cin >> temp2;
+	//cout << temp2 << endl;
+	while(1){
+            temp3=getchar();
+            if(temp3 =='\n') break;
+         }
+	if(temp1=='R' && temp2=='S') out[i]="Player1";
+	else if(temp1=='R' && temp2=='P') out[i]="Player2";
+	else if(temp1=='S' && temp2=='P') out[i]="Player1";
+	else if(temp1=='S' && temp2=='R') out[i]="Player2";
+	else if(temp1=='P' && temp2=='R') out[i]="Player1";
+	else if(temp1=='R' && temp2=='S') out[i]="Player2";
+	else out[i]="Tie";
+
+    }
+    for(i=0; i<N; i++){
+        cout << out[i] <<endl;
     }
     return 0;
 }
